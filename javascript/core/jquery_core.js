b856e9941ac94d27dc8ddf3f5390dd11804cf81d
Kris Skarbo <kris.skarbo@gmail.com>
2012-05-30 10:28:09 +0200

    Added jQuery input hint method

--- javascript/core/jquery_core.js
+++ javascript/core/jquery_core.js
@@ -1,4 +1,7 @@
 (function($) {
+	
+	// SERIALIZE FORM TO JSON
+	
 	$.fn.serializeJSON = function() {
 		var json = {};
 		jQuery.map($(this).serializeArray(), function(n, i) {
@@ -6,4 +9,76 @@
 		});
 		return json;
 	};
+	
+	// /SERIALIZE FORM TO JSON
+
+	// INPUT HINT
+
+	var INPUTHINT_CLASS = "hint";
+	var INPUTHINT_DATA = "inputHint";
+
+	var inputhintMethods = {
+		init : function(options) {
+			var data = $(this).data(INPUTHINT_DATA);
+			
+			// Data init
+			if (!data) {
+				options = $.extend({
+					"class" : INPUTHINT_CLASS
+				}, options);					
+				$(this).data(INPUTHINT_DATA, {
+					options : options
+				});				
+				data = $(this).data(INPUTHINT_DATA);
+			}
+
+			return this.each(function() {
+				var $input = jQuery(this), title = $input.attr("title"), $form = jQuery(this.form), $win = jQuery(window), hintClass = data.options.class;
+
+				function remove() {
+					if ($input.val() === title && $input.hasClass(hintClass)) {
+						$input.val("").removeClass(hintClass);
+					}
+				}
+
+				if (title) {
+					$input.blur(function() {
+						if (this.value === "") {
+							$input.val(title).addClass(hintClass);
+						}
+					}).focus(remove).blur();
+
+					$form.submit(remove);
+					$win.unload(remove);
+				}
+			});
+			
+		},
+		value : function(value) {
+			var $this = $(this), data = $this.data(INPUTHINT_DATA);
+			value = value || "";
+
+			$this.each(function(i) {
+				$(this).val(value);
+				if (value != "") {
+					$(this).val(value).removeClass(data.options.class);
+				}
+			});
+		}
+	};
+
+	$.fn.inputHint = function(method) {
+
+		if (inputhintMethods[method]) {
+			return inputhintMethods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+		} else if (typeof method === 'object' || !method) {
+			return inputhintMethods.init.apply(this, arguments);
+		} else {
+			$.error('Method ' + method + ' does not exist on jQuery.inputHint');
+		}
+
+	};
+
+	// /INPUT HINT
+
 })(jQuery);
